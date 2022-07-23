b04a3ba1c203b374ea3dd3bdc956b9e962002159
sherry032 <gemini2100@gmail.com>
2022-07-22 22:44:46 -0400

    log added

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -4,7 +4,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 exports.handler = async (event) => {
   try {
     const { amount } = JSON.parse(event.body);
-
+    console.log(amount)
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
       currency: "usd",
